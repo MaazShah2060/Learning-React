7c92ee4505c4ea4a4a28726a6c0abc1eb2b83279
Maaz Shah <58365477+MaazShah2060@users.noreply.github.com>
2020-06-08 23:20:55 +0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Learning-React
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var Items = 3
+
+ReactDOM.render(
+<div>
+  <h1>My food</h1>
+  <p>Number of itmes in list {Items}</p>
+  <ul>
+    <li>Machli</li>
+    <li>Goshat</li>
+    <li>Sabzi</li>
+    </ul>
+    </div>, 
+    document.getElementById('root'));
