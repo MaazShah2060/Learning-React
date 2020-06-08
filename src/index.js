4e348912cdf21952189a16f6ef328db159e05d70
Maaz Shah <58365477+MaazShah2060@users.noreply.github.com>
2020-06-08 23:47:28 +0500

    Working with JSX and JavaScript expressions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-var Items = 3
+const fname = "Maaz";
+const lname = "Shah";
+var rand = Math.floor(Math.random() * 11);
 
 ReactDOM.render(
-<div>
-  <h1>My food</h1>
-  <p>Number of itmes in list {Items}</p>
-  <ul>
-    <li>Machli</li>
-    <li>Goshat</li>
-    <li>Sabzi</li>
-    </ul>
-    </div>, 
-    document.getElementById('root'));
+  <div>
+    <h1>
+      {fname} {lname}
+    </h1>
+    <p>This is a random number {rand}</p>
+  </div>,
+  document.getElementById("root")
+);
