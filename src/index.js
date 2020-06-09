dcf01ec197ef7618b9e63948e14b1d71f7c55f29
Maaz Shah <58365477+MaazShah2060@users.noreply.github.com>
2020-06-09 20:07:46 +0500

    Using Date function in react

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 
 const fname = "Maaz";
 const lname = "Shah";
-var rand = Math.floor(Math.random() * 11);
+
+var d = new Date();
+var date = d.getFullYear();
 
 ReactDOM.render(
   <div>
-    <h1>
-      {fname} {lname}
-    </h1>
-    <p>This is a random number {rand}</p>
+    <h2>Created by {fname + " " + lname}</h2>
+    <p>copyrights {date}</p>
   </div>,
   document.getElementById("root")
 );
