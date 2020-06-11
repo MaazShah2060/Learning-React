fcc5f590aedfca6e7b3ee66301cfe4ab0d6175a3
Maaz Shah <msmunami63@gmail.com>
2020-06-11 17:02:45 +0500

    React components updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading";
-import List from "./List";
+import App from "./component/App.jsx"
 
 ReactDOM.render(
   <div>
-  <Heading />
-  <List />
+  <App />
   </div>,
   document.getElementById("root")
 );
