d692e75e3cd02645c4bf0bbca3ceb696960ea7de
Maaz Shah <msmunami63@gmail.com>
2020-06-12 23:47:02 +0500

    Importing and Exporting multiple functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./component/App.jsx"
+import * as Calculator from "./calculator";
 
 ReactDOM.render(
-  <App />,
+  <ul>
+    <li>{Calculator.add(1, 2)}</li>
+    <li>{Calculator.multiply(2, 3)}</li>
+    <li>{Calculator.subtract(7, 2)}</li>
+    <li>{Calculator.divide(5, 2)}</li>
+  </ul>,
   document.getElementById("root")
 );
