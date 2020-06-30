ed226a3bd8fca07fb3ef7d87c5ec19af87101155
Maaz Shah <msmunami63@gmail.com>
2020-06-30 21:25:54 +0500

    Introduction to React Hooks --> useState()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
-import Form from "./Form";
-
-var userIsRegistered = false;
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+  function reset() {
+    setCount(count - count);
+  }
   return (
     <div className="container">
-      <Form />
+      <h1>{count}</h1>
+      <button className="remove" onClick={decrease}>
+        -
+      </button>
+      <button className="add" onClick={increase}>
+        +
+      </button>
+      <button className="delete" onClick={reset}>
+        x
+      </button>
     </div>
   );
 }
 
 export default App;
-export {userIsRegistered};
