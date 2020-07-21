a86383b19d2817a213269066cde6fdf65af47c1e
MaazShah2060 <msmunami63@gmail.com>
2020-07-22 04:23:24 +0500

    useState() pracice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,47 @@
 import React, { useState } from "react";
 
 function App() {
-const [fullname,setfullname] = useState({
-  fname: "",
-  lname: ""
-});
-  function updatefullname(event){
-    const {value, name} = event.target;
-    
-    setfullname(prevValue => {
-      if(name === "fName"){
-        return {
-          fname: value,
-          lname: prevValue.lname
-        }
-      } else if(name === "lName"){
-        return {
-          fname: prevValue.fname,
-          lname: value
-        };
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function update(event){
+    const {value,name} = event.target;
+  setContact(preValue => {
+    if(name === "fName"){
+      return {
+        fName: value,
+        lName: preValue.lName,
+        email: preValue.email 
       }
-    });
+    } else if(name === "lName"){
+      return {
+        fName: preValue.fName,
+        lName: value,
+        email: preValue.email 
+      }
+    } else if(name === "email"){
+      return {
+        fName: preValue.fName,
+        lName: preValue.lName,
+        email: value 
+      }
+    } 
+  });
+
   }
   return (
     <div className="container">
-      <h1>Hello {fullname.fname} {fullname.lname}</h1>
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
       <form>
-        <input onChange={updatefullname} 
-        name="fName" 
-        placeholder="First Name" 
-        value={fullname.fname}
-        />
-        <input 
-        onChange={updatefullname} 
-        name="lName" placeholder="Last Name" 
-        value={fullname.lname}
-        />
+        <input onChange={update} name="fName" placeholder="First Name" />
+        <input onChange={update} name="lName" placeholder="Last Name" />
+        <input onChange={update} name="email" placeholder="Email" />
         <button>Submit</button>
       </form>
     </div>
