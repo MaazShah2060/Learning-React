abfda5dcb500d668d01b38fff6dddf4880b353e2
MaazShah2060 <msmunami63@gmail.com>
2020-07-29 02:59:37 +0500

    Styling tree components

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 function List(props) {
-  return <li>{props.li}</li>;
+  const [isclicked, setisclicked] = useState(false);
+  function handleclick(){
+    setisclicked(preValue => {
+      return !preValue;
+    });
+  }
+  return <li onclick={handleclick} style={{textDecoration: isclicked ? "line-through" : "none"}}>{props.li}</li>;
 }
 export default List;
